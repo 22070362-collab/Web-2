aacdf31cc140b3682d49e167b03f311a9ad94b07
Bun445 <140651097+Bun445@users.noreply.github.com>
2026-05-29 13:11:58 +0700

    Revert "Merge branch 'agents/frontend-redesign-nike-style' into Nam"
    This reverts commit a66423a2810c5d34d01e232a794c5405952bb076, reversing
    changes made to 4890ff949d36572720c4b58505dcb00b840214e1.

--- frontend-php/api/remove_from_cart.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(session_status()===PHP_SESSION_NONE) session_start();
-if($_SERVER['REQUEST_METHOD']==='POST'){
-  $id = isset($_POST['id'])? (int)$_POST['id']:0;
-  if($id && isset($_SESSION['cart'][$id])) unset($_SESSION['cart'][$id]);
-}
-header('Location: /frontend-php/pages/cart.php');
--- frontend-php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Edit these constants for your environment
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'shop_db');
-define('DB_USER', 'db_user');
-define('DB_PASS', 'db_pass');
-
-// Base URL for links (adjust if in subfolder)
-define('BASE_URL', '/frontend-php');
